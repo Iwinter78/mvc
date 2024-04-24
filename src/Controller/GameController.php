4592f7550092f6fb0c28b26385b9e853c8daaf71
Iwinter78 <kille1404.so@gmail.com>
2024-04-24 15:11:08 +0200

    added routes for main game, stand and hit. Also fixed main game route

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -7,6 +7,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Anax\TextFilter\TextFilter;
+use App\BlackJackClass\BlackJack;
+use App\BlackJackClass\Player;
 
 class GameController extends AbstractController {
     #[Route("/game", name: "game")]
@@ -29,8 +31,63 @@ class GameController extends AbstractController {
     }
 
     #[Route("/blackjack", name: "blackjack")]
-    public function blackjack(): Response
+    public function blackjack(Request $request, SessionInterface $session): Response
     {
-        return $this->render('blackjack.html.twig');
+        $blackjack = $session->get('blackjack');
+        if ($blackjack === null) {
+            $blackjack = new BlackJack();
+            $blackjack->startGame();
+            $session->set('blackjack', $blackjack);
+        }
+        
+        $session->set('blackjack', $blackjack);
+        $session->set('player', $blackjack->getPlayer());
+        $session->set('dealer', $blackjack->getDealer());
+        $session->set('stand', false);
+        $session->set('hit', false);
+    
+        $data = [
+            "player" => $session->get('player'),
+            "playerScore" => $blackjack->calculateScore($blackjack->getPlayer()->getHand()),
+            "dealer" => $session->get('dealer'),
+            "dealerScore" => $blackjack->calculateScore($blackjack->getDealer()->getHand()),
+        ];
+        return $this->render('blackjack.html.twig', $data);
+    }
+
+    #[Route(name: "blackjack_hit")]
+    public function blackjackHit(Request $request, SessionInterface $session): Response
+    {
+        $session->set('hit', true);
+        $data = [];
+        if (gettype($session->get('hit') == 'boolean')) {
+            $blackjack = new BlackJack();
+            $player = $session->get('player');
+            $dealer = $session->get('dealer');
+            $blackjack->playersTurn();
+            $data = [
+                "player" => $player,
+                "dealer" => $dealer,
+            ];
+        }
+        return $this->redirectToRoute('blackjack', $data);
+    }
+
+    #[Route(name: "blackjack_stand")]
+    public function blackjackStand(Request $request, SessionInterface $session): Response
+    {
+        $session->set('stand', true);
+        $data = [];
+        if (gettype($session->get('stand') == 'boolean')) {
+            $blackjack = new BlackJack();
+            $player = $session->get('player');
+            $dealer = $session->get('dealer');;
+            $blackjack->dealersTurn();
+            $data = [
+                "player" => $player,
+                "dealer" => $dealer,
+            ];
+        }
+        return $this->redirectToRoute('blackjack', $data);
     }
 }
