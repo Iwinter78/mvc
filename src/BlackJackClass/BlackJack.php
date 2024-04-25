25946f3bad2ba43f422837f80155ca666364eae0
Iwinter78 <kille1404.so@gmail.com>
2024-04-25 10:58:02 +0200

    added new method for dealer

--- src/BlackJackClass/BlackJack.php
+++ src/BlackJackClass/BlackJack.php
@@ -8,11 +8,13 @@ Class BlackJack {
     private $player;
     private $dealer;
     private $deck;
+    private $secondCardDealer;
 
     public function __construct() {
         $this->player = new Player();
         $this->dealer = new Player();
         $this->deck = new Deck();
+        $this->secondCardDealer = [];
     }
 
     public function getPlayer(): Player {
@@ -30,7 +32,8 @@ Class BlackJack {
     public function startGame(): void {
         $this->deck->shuffleDeck();
         $this->player->setHand($this->deck->drawCards(2));
-        $this->dealer->setHand($this->deck->drawCards(2));
+        $this->dealer->setHand($this->deck->drawCards(1));
+        $this->secondCardDealer = $this->deck->drawCards(1);
     }
 
     public function calculateScore($hand): int {
@@ -39,7 +42,12 @@ Class BlackJack {
         $hand = $this->deck->toRawData($hand);
     
         foreach ($hand as $card) {
-            $value = substr($card, 0, 1);
+            if (substr($card, 0, 2) == '10') {
+                $value = '10';
+            } else {
+                $value = substr($card, 0, 1);
+            }
+
             if (is_numeric($value)) {
                 $score += $value;
             } else if ($value == 'A') {
@@ -58,27 +66,38 @@ Class BlackJack {
         return $score;
     }
 
-    public function playersTurn(): bool {
-        while ($this->player->getScore() < 21) {
+    public function dealCard(): void {
+        if ($this->player->getScore() < 21) {
             $this->player->setHand(array_merge($this->player->getHand(), $this->deck->drawCards(1)));
             $this->player->setScore($this->calculateScore($this->player->getHand()));
-            if ($this->player->getScore() >= 21) {
-                break;
-                return false;
-            }
         }
-        return true;
     }
 
-    public function dealersTurn(): bool {
+    private function revealSecondCardDealer(): void {
+        $this->dealer->setHand(array_merge($this->dealer->getHand(), $this->secondCardDealer));
+        $this->dealer->setScore($this->calculateScore($this->dealer->getHand()));
+    }
+
+    public function stand(): void {
+        $this->revealSecondCardDealer();
         while ($this->dealer->getScore() < 17) {
             $this->dealer->setHand(array_merge($this->dealer->getHand(), $this->deck->drawCards(1)));
             $this->dealer->setScore($this->calculateScore($this->dealer->getHand()));
-            if ($this->dealer->getScore() >= 17) {
-                break;
-                return false;
-            }
-            return true;
+        }
+    }
+    public function compareResults(): string {
+        $playerScore = $this->player->getScore();
+        $dealerScore = $this->dealer->getScore();
+        if ($playerScore > 21) {
+            return 'Du förlorade!';
+        } else if ($dealerScore > 21) {
+            return 'Banken förlorade, du vinner!';
+        } else if ($playerScore > $dealerScore) {
+            return 'Du vann!';
+        } else if ($playerScore < $dealerScore) {
+            return 'Du förlorade!';
+        } else {
+            return 'Lika!';
         }
     }
 }
