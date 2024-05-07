eb09d9eb029e4b7e22f8aa8cadeb5a5ee8f6aa95
Iwinter78 <kille1404.so@gmail.com>
2024-05-07 16:06:57 +0200

    added api controller for library

---
+++ src/Controller/LibraryControllerJSON.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Repository\BooksRepository;
+use App\Entity\Books;
+
+class LibraryControllerJSON extends AbstractController
+{
+    #[Route('api/library/books', name: 'api_library')]
+    public function index(BooksRepository $bookRepository): Response
+    {
+        $books = $bookRepository->findAll();
+        $booksArray = [];
+
+        foreach ($books as $book) {
+            $booksArray[] = [
+                'id' => $book->getId(),
+                'name' => $book->getName(),
+                'isbn' => $book->getIsbn(),
+                'author' => $book->getAuthor(),
+                'image' => $book->getImage(),
+            ];
+        }
+
+        return new JsonResponse($booksArray);
+    }
+
+    #[Route('api/library/book/{isbn}', name: 'api_library_single_book')]
+    public function showSingleBook($isbn, BooksRepository $bookRepository): Response
+    {
+        $book = $bookRepository->findOneBy(['isbn' => $isbn]);
+
+        $bookArray = [
+            'id' => $book->getId(),
+            'name' => $book->getName(),
+            'isbn' => $book->getIsbn(),
+            'author' => $book->getAuthor(),
+            'image' => $book->getImage(),
+        ];
+
+        return new JsonResponse($bookArray);
+    }
+}
