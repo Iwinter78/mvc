10e7b7d612ff79982bea0c6dd2d9c919ca943e8e
Iwinter78 <kille1404.so@gmail.com>
2024-05-15 11:16:57 +0200

    tests for LibraryController

--- tests/Controller/LibraryControllerTest.php
+++ tests/Controller/LibraryControllerTest.php
@@ -17,4 +17,51 @@ class LibraryControllerTest extends WebTestCase
         $this->assertSelectorExists('section');
         $this->assertSelectorExists('a');
     }
+
+    public function testAddBook(): void
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/library/addbook');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorExists('form');
+    }
+
+    public function testShowBooks(): void
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/library/showbooks');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorExists('section');
+        $this->assertSelectorExists('div');
+        $this->assertSelectorExists('a');
+        $this->assertSelectorExists('h2');
+    }
+
+    public function testShowSingleBook(): void
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/library/showsbook/Hungerspelen');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorExists('section');
+        $this->assertSelectorExists('div');
+        $this->assertSelectorExists('a');
+        $this->assertSelectorExists('h2');
+    }
+
+    public function testEditBook(): void
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/library/editbook/9789179751221');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorExists('form');
+        $this->assertSelectorExists('input');
+    }
 }
