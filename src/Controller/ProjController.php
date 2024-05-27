9807449efb4ccbe774ab56189e01118a335d9d0e
Iwinter78 <kille1404.so@gmail.com>
2024-05-27 10:58:49 +0200

    added hit route

--- src/Controller/ProjController.php
+++ src/Controller/ProjController.php
@@ -8,6 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Anax\TextFilter\TextFilter;
+use App\BlackJackClass\AdvancedBlackJack;
 
 class ProjController extends AbstractController
 {
@@ -18,20 +19,44 @@ class ProjController extends AbstractController
     }
 
     #[Route('proj/advancedblackjack/{players}/{deck}', name: 'advancedblackjack')]
-    public function start(int $players, int $deck, Request $request, SessionInterface $session): Response
+    public function start(string $players, string $deck, Request $request, SessionInterface $session): Response
     {   
+        $players = (int)$players;
+        $deck = (int)$deck;
 
         $advancedBlackJack = new AdvancedBlackJack($players, $deck);
         $advancedBlackJack->startGame();
         $session->set('advancedBlackJack', $advancedBlackJack);
 
+        $players = $advancedBlackJack->getPlayers();
+        $dealer = $advancedBlackJack->getDealer();
+
         $data = [
             "players" => $advancedBlackJack->getPlayers(),
             "dealer" => $advancedBlackJack->getDealer(),
-            "deck" => $advancedBlackJack->getDeck(),
-            "secondCardDealer" => $advancedBlackJack->getSecondCardDealer()
+            "currentCount" => $advancedBlackJack->calculateTotalCount($players, $dealer)
         ];
 
         return $this->render('proj/advancedblackjack.html.twig', $data);
     }
+
+    //#[Route('proj/advancedblackjack/hit', name: 'hit')]
+    //public function hit(Request $request, SessionInterface $session): Response
+    //{
+    //    $advancedBlackJack = $session->get('advancedBlackJack');
+    //    $players = $advancedBlackJack->getPlayers();
+    //    $dealer = $advancedBlackJack->getDealer();
+    //    $getIndex = $request->query->get('playerIndex');
+//
+    //    $advancedBlackJack->hit($players[$getIndex]);
+    //    $session->set('advancedBlackJack', $advancedBlackJack);
+//
+    //    $data = [
+    //        "players" => $advancedBlackJack->getPlayers(),
+    //        "dealer" => $advancedBlackJack->getDealer(),
+    //        "currentCount" => $advancedBlackJack->calculateTotalCount($players, $dealer)
+    //    ];
+//
+    //    return $this->render('proj/advancedblackjack.html.twig', $data);
+    //}
 }
